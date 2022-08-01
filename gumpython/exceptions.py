f0e2d3ff4886853402ed4aafa040498178a9cf40
Wasi Haider <wsi.haidr@gmail.com>
2022-08-01 19:11:09 +0500

    :goal_net: Added exceptions

---
+++ gumpython/exceptions.py
@@ -0,0 +1,15 @@
+class GumPythonError(Exception):
+    def __init__(self, message):
+        super().__init__()
+        self.message = message
+
+    def __str__(self):
+        return f"{self.message}"
+
+
+class InputError(GumPythonError):
+    pass
+
+
+class BorderInputError(InputError):
+    pass
