9ab815ba984151dfa04ff39dc7be3d29131058c0
Wasi Haider <wsi.haidr@gmail.com>
2022-08-02 16:55:16 +0500

    Add help inputs

---
+++ gumpython/inputs/help_inputs/border.py
@@ -0,0 +1,17 @@
+NONE = "none"
+HIDDEN = "hidden"
+NORMAL = "normal"
+ROUNDED = "rounded"
+THICK = "thick"
+DOUBLE = "double"
+
+
+def all_border_types():
+    return [
+        NONE,
+        NORMAL,
+        HIDDEN,
+        ROUNDED,
+        THICK,
+        DOUBLE
+    ]
