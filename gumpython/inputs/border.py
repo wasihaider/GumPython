cd3a029afd55ed89efbeb956c9ce354ed241ff3f
Wasi Haider <wsi.haidr@gmail.com>
2022-08-02 14:09:11 +0500

    Update border input class

--- gumpython/inputs/border.py
+++ gumpython/inputs/border.py
@@ -26,8 +26,12 @@ class Border(Input):
     def __init__(self, border_type):
         self.border_type = border_type
 
+    def is_valid(self):
+        return self._validate()
+
     def _validate(self):
-        super(Border, self)._validate()
+        if not isinstance(self.border_type, str):
+            raise BorderInputError(f"'str' input expected '{type(self.border_type).__name__}' given")
         if self.border_type not in all_border_types():
             raise BorderInputError(f"border must be one of {all_border_types()} but got '{self.border_type}'")
         return True
