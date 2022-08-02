29c3fcd281e7255b9447099d4d446cf4fa3cdb38
Wasi Haider <wsi.haidr@gmail.com>
2022-08-02 17:34:02 +0500

    Add basic input classes

---
+++ gumpython/inputs/basic.py
@@ -0,0 +1,36 @@
+from .inputs import Input
+from gumpython.exceptions import InputError
+
+
+class IntegerInput(Input):
+    def __init__(self, value: int, flag_name):
+        self.value = value
+        self.flag_name = flag_name
+
+    def is_valid(self):
+        return self._validate()
+
+    def _validate(self):
+        if isinstance(self.value, int):
+            return True
+        raise InputError(f"'int' input expected for {self.flag_name} but given '{type(self.value).__name__}'")
+
+    def compile(self):
+        return self.value
+
+
+class StringInput(Input):
+    def __init__(self, value: str, flag_name):
+        self.value = value
+        self.flag_name = flag_name
+
+    def is_valid(self):
+        return self._validate()
+
+    def _validate(self):
+        if isinstance(self.value, str):
+            return True
+        raise InputError(f"'str' input expected for {self.flag_name} but given '{type(self.value).__name__}'")
+
+    def compile(self):
+        return self.value
