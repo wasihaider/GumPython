6a8fd6eb74d9de112fc82936e8c0426de98a6b14
Wasi Haider <wsi.haidr@gmail.com>
2022-08-02 18:07:41 +0500

    Add position input class

--- gumpython/inputs/position.py
+++ gumpython/inputs/position.py
@@ -1,5 +1,5 @@
 from inputs import Input
-from gumpython.exceptions import AlignmentInputError
+from gumpython.exceptions import PositionInputError
 
 
 class Position(Input):
@@ -12,12 +12,12 @@ class Position(Input):
 
     def _validate(self):
         if not isinstance(self.value, tuple):
-            raise AlignmentInputError(
+            raise PositionInputError(
                 f"'tuple' is expected for {self.flag_name} but '{type(self.value).__name__}' is given.")
         if len(self.value) != 2:
-            raise AlignmentInputError(f"'(x, y)' expected for {self.flag_name} but '{self.value}' is given.")
+            raise PositionInputError(f"'(x, y)' expected for {self.flag_name} but '{self.value}' is given.")
         if not isinstance(self.value[0], int) or not isinstance(self.value[1], int):
-            raise AlignmentInputError(f"'int' values expected for {self.flag_name}.")
+            raise PositionInputError(f"'int' values expected for {self.flag_name}.")
         return True
 
     def compile(self):
