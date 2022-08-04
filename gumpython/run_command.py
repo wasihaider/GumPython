8d96f54342b46a996d09d9e22d863bf7ea347c92
Wasi Haider <wsi.haidr@gmail.com>
2022-08-04 18:49:49 +0500

    Remove extra print

--- gumpython/run_command.py
+++ gumpython/run_command.py
@@ -2,6 +2,5 @@ import subprocess
 
 
 def run_command(cmd):
-    print(f"Going to run this command: {' '.join(cmd)}")
     response = subprocess.run(cmd, stdout=subprocess.PIPE, text=True)
     print(response.stdout)
