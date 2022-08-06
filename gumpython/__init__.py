b14bfa3e6a4767e855b5b7b18388058dbac06596
Wasi Haider <wsi.haidr@gmail.com>
2022-08-06 19:01:35 +0500

    Make version callable

--- gumpython/__init__.py
+++ gumpython/__init__.py
@@ -2,3 +2,5 @@ from gumpython.commands import Choose, Style
 from gumpython.inputs.help_inputs import alignment_types as alignment
 from gumpython.inputs.help_inputs import border_types as border
 from gumpython.inputs.help_inputs import color_names as color
+
+from .__version__ import version as __version__
