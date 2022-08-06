f8b43892860575c9f40cb4f25b4a49a802127957
Wasi Haider <wsi.haidr@gmail.com>
2022-08-06 19:48:58 +0500

    Move common flags to parent class for choose argument

--- gumpython/flags/choose_flags.py
+++ gumpython/flags/choose_flags.py
@@ -1,3 +1,5 @@
+from abc import abstractmethod
+
 from gumpython.constants import FlagSeparatorChoice, FlagTypeChoice
 
 from .flags import Flag, SubFlag
@@ -5,36 +7,23 @@ from .flags import Flag, SubFlag
 
 class ChooseFlag:
     def __init__(self):
-        self.cursor_flag_name = "cursor"
-
-
-class ChooseCursorFlag(ChooseFlag):
-    def __init__(self):
-        super().__init__()
-        # main
-        self.cursor = Flag(name=self.cursor_flag_name, type=FlagTypeChoice.STR)
-        self.prefix = SubFlag(
-            name=self.cursor_flag_name,
-            type=FlagTypeChoice.STR,
-            sub_name="prefix",
-            separator=FlagSeparatorChoice.HYPHEN,
-        )
+        self.flag_name = self.get_flag_name()
 
         # align
         self.align = SubFlag(
-            name=self.cursor_flag_name,
+            name=self.flag_name,
             type=FlagTypeChoice.ALIGN,
             sub_name="align",
             separator=FlagSeparatorChoice.DOT,
         )
         self.margin = SubFlag(
-            name=self.cursor_flag_name,
+            name=self.flag_name,
             type=FlagTypeChoice.POSITION,
             sub_name="margin",
             separator=FlagSeparatorChoice.DOT,
         )
         self.padding = SubFlag(
-            name=self.cursor_flag_name,
+            name=self.flag_name,
             type=FlagTypeChoice.POSITION,
             sub_name="padding",
             separator=FlagSeparatorChoice.DOT,
@@ -42,13 +31,13 @@ class ChooseCursorFlag(ChooseFlag):
 
         # color
         self.background = SubFlag(
-            name=self.cursor_flag_name,
+            name=self.flag_name,
             type=FlagTypeChoice.COLOR,
             sub_name="background",
             separator=FlagSeparatorChoice.DOT,
         )
         self.foreground = SubFlag(
-            name=self.cursor_flag_name,
+            name=self.flag_name,
             type=FlagTypeChoice.COLOR,
             sub_name="foreground",
             separator=FlagSeparatorChoice.DOT,
@@ -56,31 +45,31 @@ class ChooseCursorFlag(ChooseFlag):
 
         # style
         self.bold = SubFlag(
-            name=self.cursor_flag_name,
+            name=self.flag_name,
             type=FlagTypeChoice.BOOL,
             sub_name="bold",
             separator=FlagSeparatorChoice.DOT,
         )
         self.faint = SubFlag(
-            name=self.cursor_flag_name,
+            name=self.flag_name,
             type=FlagTypeChoice.BOOL,
             sub_name="faint",
             separator=FlagSeparatorChoice.DOT,
         )
         self.italic = SubFlag(
-            name=self.cursor_flag_name,
+            name=self.flag_name,
             type=FlagTypeChoice.BOOL,
             sub_name="italic",
             separator=FlagSeparatorChoice.DOT,
         )
         self.strikethrough = SubFlag(
-            name=self.cursor_flag_name,
+            name=self.flag_name,
             type=FlagTypeChoice.BOOL,
             sub_name="strikethrough",
             separator=FlagSeparatorChoice.DOT,
         )
         self.underline = SubFlag(
-            name=self.cursor_flag_name,
+            name=self.flag_name,
             type=FlagTypeChoice.BOOL,
             sub_name="underline",
             separator=FlagSeparatorChoice.DOT,
@@ -88,19 +77,19 @@ class ChooseCursorFlag(ChooseFlag):
 
         # border
         self.border = SubFlag(
-            name=self.cursor_flag_name,
+            name=self.flag_name,
             type=FlagTypeChoice.BORDER,
             sub_name="border",
             separator=FlagSeparatorChoice.DOT,
         )
         self.border_background = SubFlag(
-            name=self.cursor_flag_name,
+            name=self.flag_name,
             type=FlagTypeChoice.COLOR,
             sub_name="border-background",
             separator=FlagSeparatorChoice.DOT,
         )
         self.border_foreground = SubFlag(
-            name=self.cursor_flag_name,
+            name=self.flag_name,
             type=FlagTypeChoice.COLOR,
             sub_name="border-foreground",
             separator=FlagSeparatorChoice.DOT,
@@ -108,14 +97,34 @@ class ChooseCursorFlag(ChooseFlag):
 
         # size
         self.height = SubFlag(
-            name=self.cursor_flag_name,
+            name=self.flag_name,
             type=FlagTypeChoice.INT,
             sub_name="height",
             separator=FlagSeparatorChoice.DOT,
         )
         self.width = SubFlag(
-            name=self.cursor_flag_name,
+            name=self.flag_name,
             type=FlagTypeChoice.INT,
             sub_name="width",
             separator=FlagSeparatorChoice.DOT,
         )
+
+    @abstractmethod
+    def get_flag_name(self):
+        return None
+
+
+class ChooseCursorFlag(ChooseFlag):
+    def __init__(self):
+        super().__init__()
+        # main
+        self.cursor = Flag(name=self.flag_name, type=FlagTypeChoice.STR)
+        self.prefix = SubFlag(
+            name=self.flag_name,
+            type=FlagTypeChoice.STR,
+            sub_name="prefix",
+            separator=FlagSeparatorChoice.HYPHEN,
+        )
+
+    def get_flag_name(self):
+        return "cursor"
