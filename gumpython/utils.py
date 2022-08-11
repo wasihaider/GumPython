e68d66400685e7813b104feb6522b040e9f1e193
Wasi Haider <wsi.haidr@gmail.com>
2022-08-11 17:25:42 +0500

    Add get_input_object method

--- gumpython/utils.py
+++ gumpython/utils.py
@@ -8,6 +8,8 @@ from gumpython.inputs import (
     StringInput,
 )
 
+from .constants import FlagTypeChoice
+
 
 def get_color_object(value):
     if isinstance(value, str):
@@ -37,3 +39,18 @@ def get_alignment_object(value, flag_name):
 
 def get_position_object(value, flag_name):
     return Position(value, flag_name)
+
+
+FLAG_TYPE_INPUT_MAPPING = {
+    FlagTypeChoice.COLOR: get_color_object,
+    FlagTypeChoice.INT: get_integer_input_object,
+    FlagTypeChoice.STR: get_string_input_object,
+    FlagTypeChoice.ALIGN: get_alignment_object,
+    FlagTypeChoice.POSITION: get_position_object,
+    FlagTypeChoice.BORDER: get_border_object,
+}
+
+
+def get_input_object(flag_type, value, flag_name):
+    func = FLAG_TYPE_INPUT_MAPPING.get(flag_type)
+    return func(value, flag_name)
