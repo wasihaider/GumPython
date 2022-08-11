5bbf5414de9ff0d977dc24986027a3e735ffa736
Wasi Haider <wsi.haidr@gmail.com>
2022-08-11 19:28:27 +0500

    Create BaseFlag class

--- gumpython/flags/choose_flags.py
+++ gumpython/flags/choose_flags.py
@@ -1,120 +1,9 @@
-from abc import abstractmethod
-
 from gumpython.constants import FlagSeparatorChoice, FlagTypeChoice
 
-from .flags import Flag, SubFlag
+from .flags import BaseFlag, Flag, SubFlag
 
 
-class ChooseFlag:
-    def __init__(self):
-        self.flag_name = self.get_flag_name()
-
-        # align
-        self.align = SubFlag(
-            name=self.flag_name,
-            type=FlagTypeChoice.ALIGN,
-            sub_name="align",
-            separator=FlagSeparatorChoice.DOT,
-        )
-        self.margin = SubFlag(
-            name=self.flag_name,
-            type=FlagTypeChoice.POSITION,
-            sub_name="margin",
-            separator=FlagSeparatorChoice.DOT,
-        )
-        self.padding = SubFlag(
-            name=self.flag_name,
-            type=FlagTypeChoice.POSITION,
-            sub_name="padding",
-            separator=FlagSeparatorChoice.DOT,
-        )
-
-        # color
-        self.background = SubFlag(
-            name=self.flag_name,
-            type=FlagTypeChoice.COLOR,
-            sub_name="background",
-            separator=FlagSeparatorChoice.DOT,
-        )
-        self.foreground = SubFlag(
-            name=self.flag_name,
-            type=FlagTypeChoice.COLOR,
-            sub_name="foreground",
-            separator=FlagSeparatorChoice.DOT,
-        )
-
-        # style
-        self.bold = SubFlag(
-            name=self.flag_name,
-            type=FlagTypeChoice.BOOL,
-            sub_name="bold",
-            separator=FlagSeparatorChoice.DOT,
-        )
-        self.faint = SubFlag(
-            name=self.flag_name,
-            type=FlagTypeChoice.BOOL,
-            sub_name="faint",
-            separator=FlagSeparatorChoice.DOT,
-        )
-        self.italic = SubFlag(
-            name=self.flag_name,
-            type=FlagTypeChoice.BOOL,
-            sub_name="italic",
-            separator=FlagSeparatorChoice.DOT,
-        )
-        self.strikethrough = SubFlag(
-            name=self.flag_name,
-            type=FlagTypeChoice.BOOL,
-            sub_name="strikethrough",
-            separator=FlagSeparatorChoice.DOT,
-        )
-        self.underline = SubFlag(
-            name=self.flag_name,
-            type=FlagTypeChoice.BOOL,
-            sub_name="underline",
-            separator=FlagSeparatorChoice.DOT,
-        )
-
-        # border
-        self.border = SubFlag(
-            name=self.flag_name,
-            type=FlagTypeChoice.BORDER,
-            sub_name="border",
-            separator=FlagSeparatorChoice.DOT,
-        )
-        self.border_background = SubFlag(
-            name=self.flag_name,
-            type=FlagTypeChoice.COLOR,
-            sub_name="border-background",
-            separator=FlagSeparatorChoice.DOT,
-        )
-        self.border_foreground = SubFlag(
-            name=self.flag_name,
-            type=FlagTypeChoice.COLOR,
-            sub_name="border-foreground",
-            separator=FlagSeparatorChoice.DOT,
-        )
-
-        # size
-        self.height = SubFlag(
-            name=self.flag_name,
-            type=FlagTypeChoice.INT,
-            sub_name="height",
-            separator=FlagSeparatorChoice.DOT,
-        )
-        self.width = SubFlag(
-            name=self.flag_name,
-            type=FlagTypeChoice.INT,
-            sub_name="width",
-            separator=FlagSeparatorChoice.DOT,
-        )
-
-    @abstractmethod
-    def get_flag_name(self):
-        return None
-
-
-class ChooseCursorFlag(ChooseFlag):
+class ChooseCursorFlag(BaseFlag):
     def __init__(self):
         super().__init__()
         # main
@@ -130,7 +19,7 @@ class ChooseCursorFlag(ChooseFlag):
         return "cursor"
 
 
-class ChooseItemFlag(ChooseFlag):
+class ChooseItemFlag(BaseFlag):
     def __init__(self):
         super(ChooseItemFlag, self).__init__()
 
@@ -138,7 +27,7 @@ class ChooseItemFlag(ChooseFlag):
         return "item"
 
 
-class ChooseSelectedFlag(ChooseFlag):
+class ChooseSelectedFlag(BaseFlag):
     def __init__(self):
         super(ChooseSelectedFlag, self).__init__()
         self.prefix = SubFlag(
